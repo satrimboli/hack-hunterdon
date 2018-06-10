c98808f87d6c956b02d9594bd49a848a4365bd43
Stephanie Trimboli <satrimboli@gmail.com>
2018-06-10 06:57:56 -0400

    fake it til you make it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  var counter = 0;
+  var interval = window.setInterval(playWaypoint, 3000);
+  var waypoints = $('.waypoint');
+  var currentWaypoint;
+
+  function playWaypoint() {
+    console.log('playing waypoint ' + counter);
+    
+    currentWaypoint = waypoints.eq(counter);
+    counter ++;
+    
+    currentWaypoint.addClass('active');
+    currentWaypoint.find('audio')[0].play();
+    currentWaypoint[0].scrollIntoView();
+  }
+
+});
