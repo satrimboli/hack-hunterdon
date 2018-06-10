d5fbb13f3cd7e907a2d0fffd72a377ca3a61d65f
Thomas Shweh <shweht@rpi.edu>
2018-06-09 20:36:33 -0400

    Added Location Module

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Platform, Text, View, Image } from 'react-native';
+import {Constants, Location, Permissions } from 'expo';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,14 +9,44 @@ export default class App extends React.Component {
       image: 'testing',
       direction: 'test',
       audio: '',
-      coordinates: ''
+      coordinates: '',
+      location: null,
+      errorMessage: null,
     }
   }
+  componentWillMount() {
+  if (Platform.OS === 'android' && !Constants.isDevice) {
+    this.setState({
+      errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+    });
+  } else {
+    this._getLocationAsync();
+  }
+}
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+    };
   render() {
+    let text = 'Waiting..';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
     return (
       <View style={styles.container}>
         <Image source={this.state.image} />
         <Text>{this.state.direction}</Text>
+        <Text style={styles.paragraph}>{text}</Text>
       </View>
     );
   }
@@ -27,5 +58,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
+    paragraph: {
+      margin: 24,
+      fontSize: 18,
+      textAlign: 'center',
+    },
 });
